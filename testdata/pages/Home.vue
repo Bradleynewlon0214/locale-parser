d4f1263b4c835651e120e04479f86c78e737a8ab
Bradleynewlon0214 <bradleynewlon@gmail.com>
2026-02-11 22:08:19 -0500

    testdata

---
+++ testdata/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ $t('pages.home.hello-world') }}</h1>
+    <p>{{ $t('pages.home.welcome-to-our-application') }}</p>
+    <button>{{ $t('pages.home.submit') }}</button>
+    <span>{{ $t('pages.home.hello-world-2') }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
